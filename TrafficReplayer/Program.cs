0124479497c77452c0e947c17d40eceacb75c7a5
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-09-05 17:14:59 +0200

    fix

--- TrafficReplayer/Program.cs
+++ TrafficReplayer/Program.cs
@@ -6,63 +6,61 @@ using System.Threading.Tasks;
 using CommandLine;
 using RestSharp;
 using static System.Console;
-#pragma warning disable 1998
 
+#pragma warning disable 1998
 
 namespace TrafficReplayer
 {
     public static class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
             Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(async options =>
+                .WithParsed(Action);
+        }
+
+        private static async void Action(Options options)
+        {
+            ShowOptions(options);
+
+            IEnumerable<string> resources = (await File.ReadAllLinesAsync(options.FileName)).ToList();
+
+            RestClient targetClient = new RestClient(options.Scope);
+
+            Parallel.ForEach(resources, new ParallelOptions { MaxDegreeOfParallelism = options.MaxDegreeOfParallelism },
+                resource =>
                 {
-                    ShowOptions(options);
-
-                    IEnumerable<string> resources = File.ReadAllLines(options.FileName).ToList();
-
-                    RestClient targetClient = new RestClient(options.Scope);
-
-                    Parallel.ForEach(resources,
-                        new ParallelOptions {MaxDegreeOfParallelism = options.MaxDegreeOfParallelism}, resource =>
-                        {
-                            IRestRequest requestTarget = new RestRequest($"{options.Scope}{resource}");
-                            WriteLine(resource);
-                            requestTarget.AddHeader("x-auth-token", options.XAuthToken);
-                            IRestResponse<Response> responseTarget = targetClient.Execute<Response>(requestTarget);
-                        });
-
-                    int i = 0;
-
-                    while (true)
-                    {
-                        IEnumerable<string> chunk = resources.Skip(i).Take(options.Chunk);
-                        List<Task> batch = new List<Task>();
-                        WriteLine("Chunk >");
-
-                        foreach (string resource in chunk)
-                        {
-                            batch.Add(Task.Run(() =>
-                            {
-                                IRestRequest requestTarget = new RestRequest($"{options.Scope}{resource}");
-                                requestTarget.AddHeader("x-auth-token", options.XAuthToken);
-                                IRestResponse<Response> responseTarget =
-                                    targetClient.Execute<Response>(requestTarget);
-
-                                WriteLine(responseTarget.IsSuccessful ? "ok" : "error");
-                            }));
-                        }
-
-                        await Task.WhenAll(batch.ToArray());
-                        WriteLine("Chunk finished. ");
-                        i += options.Chunk;
-                        if (i > resources.Count())
-                        {
-                            i = 0;
-                        }
-                    }
+                    IRestRequest requestTarget = new RestRequest($"{options.Scope}{resource}");
+                    WriteLine(resource);
+                    requestTarget.AddHeader("x-auth-token", options.XAuthToken);
+                    IRestResponse<Response> responseTarget = targetClient.Execute<Response>(requestTarget);
                 });
+
+            var i = 0;
+
+            while (true)
+            {
+                var chunk = resources.Skip(i).Take(options.Chunk);
+                List<Task> batch = [];
+                WriteLine("Chunk >");
+
+                batch.AddRange(chunk.Select(resource => Task.Run(() =>
+                {
+                    IRestRequest requestTarget = new RestRequest($"{options.Scope}{resource}");
+                    requestTarget.AddHeader("x-auth-token", options.XAuthToken);
+                    var responseTarget = targetClient.Execute<Response>(requestTarget);
+
+                    WriteLine(responseTarget.IsSuccessful ? "ok" : "error");
+                })));
+
+                await Task.WhenAll(batch.ToArray());
+                WriteLine("Chunk finished. ");
+                i += options.Chunk;
+                if (i > resources.Count())
+                {
+                    i = 0;
+                }
+            }
         }
 
         private static void ShowOptions(Options options)
